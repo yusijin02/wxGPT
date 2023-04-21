0ffc8fa1d250094625e4ed9ae32abb0fbdffb05d
yusijinfs <1207937423@qq.com>
2023-04-21 21:52:07 +0800

    build: init

---
+++ GPTAPI/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPTAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string Demo_1()
+        {
+            return "Test Pass.\nBy Sijin Yu.\n测试通过.\n余思进.";
+        }
+        [HttpGet]
+        public string Demo_2(string userName, string t)
+        {
+            return "userName=" + userName + "\nt=" + t;
+        }
+    }
+}
